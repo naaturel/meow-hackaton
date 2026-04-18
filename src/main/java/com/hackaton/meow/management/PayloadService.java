588bdeb03f476c7e29d1940377c746d90c47996d
Laurent <2-naaturel@users.noreply.gitlab.naaturel.be>
2026-04-18 18:22:43 +0200

    Implement device type filtering

--- src/main/java/com/hackaton/meow/management/PayloadService.java
+++ src/main/java/com/hackaton/meow/management/PayloadService.java
@@ -30,14 +30,15 @@ public class PayloadService {
         Map<String, Object> deviceInfo = message.getObject(new TypeToken<Map<String, Object>>(){}.getType(), "deviceInfo");
         Map<String, Object> obj = message.getObject(new TypeToken<Map<String, Object>>(){}.getType(), "object");
         List<Map<String, Object>> rxInfo = (message.getObject(new TypeToken<List<Map<String, Object>>>(){}.getType(), "rxInfo"));
+        String eui = (String)deviceInfo.get("devEui");
         Map<String, Object> res = new HashMap<>();
 
+        res.put("device_eui", eui);
         res.put("device_name", deviceInfo.get("deviceName"));
         res.put("snr", rxInfo.getFirst().get("snr"));
         res.put("rssi", rxInfo.getFirst().get("rssi"));
         res.put("time", rxInfo.getFirst().get("time"));
 
-        String eui = (String)deviceInfo.get("devEui");
         if(isGaz(eui)){
             res.put("value", obj.get("TOTAL_PULSE"));
         } else if(isWater(eui)){
@@ -49,14 +50,24 @@ public class PayloadService {
     }
 
     private boolean isGaz(String id){
-        return true;
+        return id.equalsIgnoreCase("a84041bc185f4796");
     }
 
     private boolean isWater(String id){
-        return true;
+        return id.equalsIgnoreCase("a840410e818855bf")
+            || id.equalsIgnoreCase("a84041e4018855c2")
+            || id.equalsIgnoreCase("a8404102e18855bc")
+            || id.equalsIgnoreCase("a840417df18860ac")
+            || id.equalsIgnoreCase("a8404152d35d680a")
+            || id.equalsIgnoreCase("a84041f09188609e");
     }
 
     private boolean isElectricity(String id){
-        return true;
+        return id.equalsIgnoreCase("102cefffe0111a7")
+            || id.equalsIgnoreCase("102ceffffe0111e4")
+            || id.equalsIgnoreCase("102cefffe011102b")
+            || id.equalsIgnoreCase("102cefffe011026")
+            || id.equalsIgnoreCase("102cefffe0111c0")
+            || id.equalsIgnoreCase("102cefffe011134");
     }
 }
